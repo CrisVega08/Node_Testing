0784c4c6213d053d44a81b7c2037b72dac8aad9b
Cristian <cristian>
2017-12-21 08:00:33 -0500

    mLab

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise;
-module.exports.connectDB = mongoose.connect('mongodb://localhost/Camarelo', { useMongoClient: true })
+module.exports.connectDB = mongoose.connect('mongodb://Cristian08:NodeAngular2017@ds161446.mlab.com:61446/caramelo'||'mongodb://localhost/Camarelo', { useMongoClient: true })
 
