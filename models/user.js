abc93a254bd1e270a293d84ba0a53531de5a0433
Cristian Vega <cristian.vega08@gmail.com>
2017-12-21 23:26:33 -0500

    user

--- models/user.js
+++ models/user.js
@@ -1,10 +1,30 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const validator = require('validator')
 
 var userSchema = new Schema({
   name:  { type: String, required: true },
-  password: { type: String, trim: true, minlength: 4, required : true },
-  email:   String,
+  password: { type: String, trim: true, minlength: 6, required : true},
+  email: { type: String, 
+    trim: true, 
+    minlength: 4, 
+    required : true, 
+    unique : true,
+    validate: {
+      validator: validator.isEmail,
+      message: '{VALUE} is not a valid email'
+    }
+  },
+  tokens:[{
+    access:{ 
+      type: String, 
+      require: true
+    },
+    token:{
+      type: String, 
+      require: true
+    }
+  }],
   date: { type: Date, default: Date.now },
   hidden: Boolean
 });
