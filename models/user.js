6e404d636506ed2e1d8f6601046badf296bf3bbb
Cristian <cristian>
2017-12-26 17:58:40 -0500

    conflicts

--- models/user.js
+++ models/user.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const validator = require('validator')
-<<<<<<< HEAD
 const jwt = require('jsonwebtoken')
 const _ = require('lodash')
 
@@ -40,34 +39,6 @@ var userSchema = new Schema({
       require: true
     }
   }]
-=======
-
-var userSchema = new Schema({
-  name:  { type: String, required: true },
-  password: { type: String, trim: true, minlength: 6, required : true},
-  email: { type: String, 
-    trim: true, 
-    minlength: 4, 
-    required : true, 
-    unique : true,
-    validate: {
-      validator: validator.isEmail,
-      message: '{VALUE} is not a valid email'
-    }
-  },
-  tokens:[{
-    access:{ 
-      type: String, 
-      require: true
-    },
-    token:{
-      type: String, 
-      require: true
-    }
-  }],
-  date: { type: Date, default: Date.now },
-  hidden: Boolean
->>>>>>> abc93a254bd1e270a293d84ba0a53531de5a0433
 });
 
 userSchema.methods.toJSON = function () {
